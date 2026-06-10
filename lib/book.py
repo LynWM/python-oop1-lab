005262371e9ed5ef9a10632f609b60a6bba96416
LynWM <murathimilynette@gmail.com>
2026-06-10 12:18:47 +0300

    feat:Added Book and Coffee classes

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,41 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
     
-        
+    @page_count.setter
+    def page_count(self, page_count):
+        if isinstance(page_count, int):
+            self._page_count = page_count
+
+        else:
+            raise ValueError("page_count must be an integer")
+
+    # Method for book => turn_page()
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+# Input calls - outside of class
+input_title = input("Enter the Book Title: ")
+input_page_count = input("Enter number of pages: ")
+
+# Converting page_count to int
+try:
+    book = Book(input_title, int(input_page_count))
+    print(f"Your added book is: \n {book.title} - {book.page_count}")
+    book.turn_page()
+
+except ValueError:
+    raise ValueError ("page_count must be an integer")
+
+
+
+
+
+
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,35 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
+
+    # Instance method
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size (self, size):
+        if size in ["Small", "Medium", "Large"]:
+            self._size = size
+        else:
+            raise ValueError("size must be Small, Medium, or Large")
+    
+    # Method for coffee => tip()
+    def tip(self):
+        print("This coffee is great here's a tip!")
+        self.price += 1
+
+
+# Input calls - outside of class
+input_size = input("What size of coffee would you like? (small/medium/large): \n ").capitalize()
+input_price = input("Enter the price of your coffee: ")
+
+coffee = Coffee(input_size, float(input_price))
+print(f"Your coffee order is: \n {coffee.size} - {coffee.price}")
+
+coffee.tip()
+print(f"New price after the tip: {coffee.price}")
