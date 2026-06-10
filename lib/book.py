aad4de65fa538ab37bd9887fa351c56239438cc3
LynWM <murathimilynette@gmail.com>
2026-06-10 12:24:07 +0300

    fix:Fixed up classes for testing

--- lib/book.py
+++ lib/book.py
@@ -8,34 +8,25 @@ class Book:
     @property
     def page_count(self):
         return self._page_count
-    
+
     @page_count.setter
     def page_count(self, page_count):
         if isinstance(page_count, int):
             self._page_count = page_count
-
         else:
-            raise ValueError("page_count must be an integer")
+            print("page_count must be an integer")
 
-    # Method for book => turn_page()
     def turn_page(self):
         print("Flipping the page...wow, you read fast!")
 
-# Input calls - outside of class
-input_title = input("Enter the Book Title: ")
-input_page_count = input("Enter number of pages: ")
 
-# Converting page_count to int
-try:
-    book = Book(input_title, int(input_page_count))
-    print(f"Your added book is: \n {book.title} - {book.page_count}")
-    book.turn_page()
+if __name__ == "__main__":
+    input_title = input("Enter the Book Title: ")
+    input_page_count = input("Enter number of pages: ")
 
-except ValueError:
-    raise ValueError ("page_count must be an integer")
-
-
-
-
-
-
+    try:
+        book = Book(input_title, int(input_page_count))
+        print(f"Title: {book.title}, Pages: {book.page_count}")
+        book.turn_page()
+    except ValueError:
+        print("page_count must be an integer")
